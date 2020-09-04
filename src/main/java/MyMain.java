95be0143161e276135c9f4fd340a151c9a3335c9
Jonah-Has <23jhasegawa@athenian.org>
2020-09-04 16:29:56 +0000

    added the scanner and wrote a few println lines

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -33,11 +33,45 @@ public class MyMain {
 
     public static void main(final String[] args) {
         // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
+        //System.out.println(median(1, 2, 3)); // should be 2
+        //System.out.println(magnitude(-7, -1)); // should be -7
+        //System.out.println(pythagoras(3, 4)); // should be 5.0
 
         final Scanner scan = new Scanner(System.in);
+        System.out.println("What function do you want to use? (type 'py' for pythagoras, 'ma' for magnitude, and 'me' for median");
+        String action = scan.next();
+
+        if (action.equals("me")){
+            System.out.println("This function takes 3 int inputs. Put one per line.");
+            System.out.println("a =");
+            int aa = scan.nextInt();
+            System.out.println("b =");
+            int bb = scan.nextInt();
+            System.out.println("c =");
+            int cc = scan.nextInt();
+            System.out.println("The median of those numbers is " + Integer.toString(median(aa,bb,cc)));
+            
+        }
+        else if (action.equals("ma")){
+            System.out.println("This function takes 2 int inputs. Put one per line.");
+            System.out.println("a =");
+            int aa = scan.nextInt();
+            System.out.println("b =");
+            int bb = scan.nextInt();
+            System.out.println("The number with the greatest absolute value here is " + Integer.toString(magnitude(aa,bb)));
+        }
+        else if (action.equals("py")){
+            System.out.println("This function takes 2 int inputs. Put one per line.");
+             System.out.println("a =");
+            int aa = scan.nextInt();
+            System.out.println("b =");
+            int bb = scan.nextInt();
+            System.out.println("The hypotenuse of a right triangle with those legs is " + Double.toString(pythagoras(aa,bb)));
+        }
+        else{
+            System.out.println("Something went wrong. Try running the program again.");
+        }
+        scan.close();
         // YOUR CODE HERE
     }
 }
